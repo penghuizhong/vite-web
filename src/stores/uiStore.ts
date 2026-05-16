1d7819aee4528e1f236edf693a0df8b780b2f342
penghuizhong <q823455376@gmail.com>
2026-05-16 22:02:49 +0800

    feat: add chatPanelOpen state to uiStore

--- src/stores/uiStore.ts
+++ src/stores/uiStore.ts
@@ -6,6 +6,9 @@ interface UiStore {
   setSidebarOpen: (open: boolean) => void
   authModalOpen: boolean
   setAuthModalOpen: (open: boolean) => void
+  chatPanelOpen: boolean
+  setChatPanelOpen: (open: boolean) => void
+  toggleChatPanel: () => void
 }
 
 export const useUiStore = create<UiStore>()((set) => ({
@@ -14,4 +17,7 @@ export const useUiStore = create<UiStore>()((set) => ({
   setSidebarOpen: (open) => set({ sidebarOpen: open }),
   authModalOpen: false,
   setAuthModalOpen: (open) => set({ authModalOpen: open }),
+  chatPanelOpen: false,
+  setChatPanelOpen: (open) => set({ chatPanelOpen: open }),
+  toggleChatPanel: () => set((state) => ({ chatPanelOpen: !state.chatPanelOpen })),
 }))
